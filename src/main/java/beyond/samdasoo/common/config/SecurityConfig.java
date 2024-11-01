19f1855eadb22d7e6f4a7f2c91efb7ee9a3e81e1
kuk329 <kuk329@cau.ac.kr>
2024-11-01 10:38:33 +0900

    시큐리티 설정 수정

--- src/main/java/beyond/samdasoo/common/config/SecurityConfig.java
+++ src/main/java/beyond/samdasoo/common/config/SecurityConfig.java
@@ -31,26 +31,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import javax.servlet.DispatcherType;
-import java.util.Arrays;
-import java.util.Collections;
 
 @RequiredArgsConstructor
 @Configuration
@@ -128,7 +108,8 @@ public class SecurityConfig {
                 "http://127.0.0.1:5173",
                 "http://localhost:5175",
                 "https://d18cdfira8jz6e.cloudfront.net",
-                "https://cdn.samdasoo.click"
+                "https://cdn.samdasoo.click",
+                "http://cdn.samdasoo.click"
         ));
         configuration.setAllowCredentials(true);
 
